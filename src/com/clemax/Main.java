3390d4aa0068aaf5b6af32491b8871181ef77c62
Clemens Rustemeier <clemensrustemeier@icloud.com>
2021-09-19 17:08:59 +0200

    Edit Main.jva

--- src/com/clemax/Main.java
+++ src/com/clemax/Main.java
@@ -3,30 +3,47 @@ package com.clemax;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Own Queue Testing");
-        System.out.println("=================");
+        queueTesting();
+    }
+
+    public static void queueTesting() {
+        System.out.println();
+        System.out.println(" === Queue Test === ");
+        System.out.println();
         System.out.println();
 
         OQueue<Integer> queue = new OQueue<>();
 
-        System.out.println("queue.front() = " + queue.front());
-        System.out.println("queue.isEmpty() = " + queue.isEmpty());
+        System.out.println("[ Queue nach dem erzeugen");
+        System.out.println("[ =======================");
+        System.out.println("[ queue.front() = " + queue.front());
+        System.out.println("[ queue.isEmpty() = " + queue.isEmpty());
+        System.out.println();
 
         Integer i = 1;
         queue.enqueue(i);
 
-        System.out.println("queue.front() = " + queue.front());
-        System.out.println("queue.isEmpty() = " + queue.isEmpty());
+        System.out.println("[ Queue nach enqueue(1)");
+        System.out.println("[ =====================");
+        System.out.println("[ queue.front() = " + queue.front());
+        System.out.println("[ queue.isEmpty() = " + queue.isEmpty());
+        System.out.println();
 
         Integer j = 5;
         queue.enqueue(j);
 
-        System.out.println("queue.front() = " + queue.front());
-        System.out.println("queue.isEmpty() = " + queue.isEmpty());
+        System.out.println("[ Queue nach enqueue(5)");
+        System.out.println("[ =====================");
+        System.out.println("[ queue.front() = " + queue.front());
+        System.out.println("[ queue.isEmpty() = " + queue.isEmpty());
+        System.out.println();
 
         queue.dequeue();
 
-        System.out.println("queue.front() = " + queue.front());
-        System.out.println("queue.isEmpty() = " + queue.isEmpty());
+        System.out.println("[ Queue nach dequeue");
+        System.out.println("[ ==================");
+        System.out.println("[ queue.front() = " + queue.front());
+        System.out.println("[ queue.isEmpty() = " + queue.isEmpty());
+        System.out.println();
     }
 }
